7d1c56a751e0dd6294109b4e11e140e4cc634e7d
Jesus Castillo <jalbert.casthern@gmail.com>
2025-02-28 13:03:11 -0600

    Update WebConfig.java

--- src/main/java/com/example/holamundo/config/WebConfig.java
+++ src/main/java/com/example/holamundo/config/WebConfig.java
@@ -1,7 +1,8 @@
 package com.example.holamundo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("forward:/index.html");
         registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
         registry.addViewController("/{x:^(?!api$).*$}/{y:[\\w\\-]+}").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permitir todas las rutas
+                .allowedOrigins("https://holamundo-frontend-production.up.railway.app")  // URL del frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
+                .allowedHeaders("*");  // Permitir todos los encabezados
     }
 }
